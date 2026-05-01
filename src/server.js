997c42f9543f57edddee9cc31dacee5cb953a15d
Arthur Sauer Germano <germanooarthuur@gmail.com>
2026-05-01 14:04:24 -0300

    feat: connect to database and create first tables

--- src/server.js
+++ src/server.js
@@ -1,13 +1,40 @@
 // @ts-check
 import express from "express";
+import { config } from "dotenv";
+import { connectDB, disconnectDB } from "./config/db.js";
 
 import movieRoutes from "./routes/movieRoutes.js";
+
+config();
+connectDB();
 
 const app = express();
 
 app.use("/movies", movieRoutes);
 
 const PORT = 5001;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error("unhandledRejection", err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+process.on("uncaughtException", async (err) => {
+  console.error("uncaughtException", err);
+  await disconnectDB();
+  process.exit(1);
+});
+
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM received, shutting down gracefully");
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(0);
+  });
 });
